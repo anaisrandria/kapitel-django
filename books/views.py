d0a48f9d6744ed01efe05717a1519cefc02747c7
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-12-19 16:39:08 +0100

    add: dotenv files

--- books/views.py
+++ books/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
+import os
 
 def get_book(request):
-    url = f'https://books.googleapis.com/books/v1/volumes?q=$changer&maxResults=40&printType=books&key=AIzaSyAMkR99i88RXvSuDZ3CmZdbk5-zt8znpGE'
+    api_key = os.getenv('API_KEY')
+    url = f'https://books.googleapis.com/books/v1/volumes?q=$changer&maxResults=40&printType=books&key={api_key}'
     response = requests.get(url)
     data = response.json()
     print('🧀 Data is:', data)
-    # return render(request, 'books/home.html', {'data': data})
     return JsonResponse(data)
--- mybookmate/settings.py
+++ mybookmate/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%-f1h&#f%(_x(d0_723qa0geyca@mr7_f#j69jmg8%5y9byvk'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
