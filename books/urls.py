1142654bf0f40d2efcd042f4c74fa68f55b6d39a
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-12-19 16:58:19 +0100

    add: fetch external api from books app

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_book, name='book_list'),
-    # path('books/<str:search_input>/', views.get_book, name='book_detail'),
+    path('', views.book_list, name='book_list'),
+    # path('books/<str:search_input>/', views.book_list, name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -3,10 +3,11 @@ from django.http import JsonResponse
 import requests
 import os
 
-def get_book(request):
+def book_list(request):
     api_key = os.getenv('API_KEY')
     url = f'https://books.googleapis.com/books/v1/volumes?q=$changer&maxResults=40&printType=books&key={api_key}'
     response = requests.get(url)
     data = response.json()
     print('🧀 Data is:', data)
     return JsonResponse(data)
+    # return render(request, 'books/home.html', {'data': data})
