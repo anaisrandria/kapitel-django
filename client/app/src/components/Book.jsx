c18a599cbdf2812d6a888f51c656fb010fed120d
anaisrandria <anais.randriamitantsoa@gmail.com>
2025-01-08 11:34:23 +0100

    add: dropdown search bar wip

--- client/app/src/components/Book.jsx
+++ client/app/src/components/Book.jsx
@@ -1,7 +1,20 @@
 function Book() {
 
+    
+
     return (
-        <h1>My Books</h1>
+        <div className="flex gap-6">
+            <img id="big-cover" src="/public/missingbook.jpg" alt="cover" className="w-1/3"/>
+            <div id="book-description" className="flex flex-col gap-3">
+                <h1 id="title" className="text-3xl font-lora">Title</h1>
+                <p id="author" className="text-xl">Auteur</p>
+                <p id="summary" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+                aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+        
     )
 }
 
--- client/app/src/components/Header.jsx
+++ client/app/src/components/Header.jsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from "react";
 
 function Header({ setInput }) {
 
-    // const [input, setInput] = useState('')
-
     const fetchGoogleBooksApi = async() => {
         const api_url = `https://books.googleapis.com/books/v1/volumes?q=${setInput}&maxResults=40&printType=books&key=${import.meta.env.VITE_API_KEY}`
         const response = await fetch(api_url)
@@ -29,13 +27,23 @@ function Header({ setInput }) {
 
     return (
         <div className="sticky z-10 top-0 w-full flex justify-stretch items-center pt-3 pb-6 bg-beige">
-            <div id="logo-searchbar" className="flex gap-4 w-full">
+            <div id="logo-search" className="flex gap-4 w-full">
                 <Link to="/">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                     </svg>
                 </Link>
-                <input type="text" placeholder="Rechercher un livre..." name="search-bar" onChange={handleInput} className="bg-inherit py-1 w-full text-xs text-slate-700 border-x-0 border-t-0 border-y border-slate-300 focus:outline-none"></input>
+                <div id="search-container" className="relative w-full border-x-0 border-t-0 border-y border-slate-300">
+                    <input type="text" placeholder="Rechercher un livre..." name="search-bar" onChange={handleInput} className="bg-inherit py-1 text-xs text-slate-700 focus:outline-none"></input>
+                    {/* Search results container */}
+                    <div className="absolute mt-1 w-full p-2 bg-white shadow-lg rounded-bl rounded-br max-h-36 overflow-y-auto text-sm">
+                        <ul>
+                            <li>Test 1</li>
+                            <li>Test 2</li>
+                            <li>Test 3</li>
+                        </ul>    
+                    </div>
+                </div>
             </div>
             <div id="menu" className="flex justify-end gap-4 w-full text-sm">
                 <Link to="#"><p>Fil d'actualité</p></Link>
