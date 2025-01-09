83f18d3fda9b9dd92eda13593103fcc0edf69de5
anaisrandria <anais.randriamitantsoa@gmail.com>
2025-01-09 11:12:44 +0100

    add: book card component

--- client/app/src/components/BookCard.jsx
+++ client/app/src/components/BookCard.jsx
@@ -1,7 +1,32 @@
-function BookCard({ books, setBooks }) {
+function BookCard({ book, setBooks, id, title, authors, release_year, status, comments }) {
+
+    const deleteBook = async (pk) => {
+        try {
+            const response = await fetch (`http://127.0.0.1:8000/api/books/${pk}`, {
+                method: "DELETE",
+            });
+
+            // await response.json();
+            setBooks((prev) => prev.filter((book) => book.id !== pk))
+
+        } catch (err) {
+            console.log(err)
+        };
+    };
 
     return (
         <>
+        <div id="book-card" className="flex-shrink-0 flex flex-col mb-8 text-sm w-44" key={book.id}>
+            {/* {" "} */}
+            <img src={book.image_link || "missingbook.jpg"} alt="book-cover" className="w-full max-h-64 drop-shadow-md rounded-sm mb-2"/>
+            <p className="font-bold">{book.title}</p>
+            <p>{book.authors}</p>
+            <p>{book.release_year}</p>
+            <p>Status: {book.status}</p>
+            <p>Comments: {book.comments}</p>
+            <button className="px-2 py-2 rounded-md my-2 border border-amber-700 bg-transparent text-amber-700 hover:bg-amber-700 hover:text-white">Mes notes</button>
+            <button onClick={() => deleteBook(book.id)} className="px-2 py-2 border bg-amber-700 text-white rounded-md">Supprimer</button>
+        </div>
         </>
     )
 }
--- client/app/src/components/Home.jsx
+++ client/app/src/components/Home.jsx
@@ -4,6 +4,7 @@ import StatusQueued from "./StatusQueued";
 import StatusCompleted from "./StatusCompleted";
 import StatusPaused from "./StatusPaused";
 import StatusArchived from "./StatusArchived";
+import BookCard from "./BookCard";
 
 function Home({ books, setBooks }) {
 
@@ -24,20 +25,6 @@ function Home({ books, setBooks }) {
             console.log(err);
         }
     };
-
-    const deleteBook = async (pk) => {
-        try {
-            const response = await fetch (`http://127.0.0.1:8000/api/books/${pk}`, {
-                method: "DELETE",
-            });
-
-            // await response.json();
-            setBooks((prev) => prev.filter((book) => book.id !== pk))
-
-        } catch (err) {
-            console.log(err)
-        };
-    };
     
     return (
         <div>
@@ -51,17 +38,17 @@ function Home({ books, setBooks }) {
             <StatusArchived />
             <div className="flex gap-6 w-[80vw] overflow-x-auto">
                 {books.map((book) => (
-                    <div id="book-card" className="flex-shrink-0 flex flex-col mb-8 text-sm w-44" key={book.id}>
-                        {/* {" "} */}
-                        <img src={book.image_link || "missingbook.jpg"} alt="book-cover" className="w-full max-h-64 drop-shadow-md rounded-sm mb-2"/>
-                        <p className="font-bold">{book.title}</p>
-                        <p>{book.authors}</p>
-                        <p>{book.release_year}</p>
-                        <p>Status: {book.status}</p>
-                        <p>Comments: {book.comments}</p>
-                        <button className="px-2 py-1 border bg-amber-700 text-white rounded-md">Modifier</button>
-                        <button onClick={() => deleteBook(book.id)} className="px-2 py-1 border bg-amber-700 text-white rounded-md">Supprimer</button>
-                    </div>
+                    <BookCard 
+                        book={book}
+                        setBooks={setBooks}
+                        key={book.id}
+                        id={book.id}
+                        title={book.title}
+                        authors={book.authors}
+                        release_year={book.release_year}
+                        status={book.status}
+                        comments={book.comments}
+                     />
                 ))}
             </div>
 
