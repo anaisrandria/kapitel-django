a66a208413c032ec2327def473bbc3f38c51ae81
anaisrandria <anais.randriamitantsoa@gmail.com>
2025-01-14 14:49:00 +0100

    add: refactoring book lists by status

--- client/app/src/components/BooksList.jsx
+++ client/app/src/components/BooksList.jsx
@@ -1,18 +1,23 @@
 import BookCard from "./BookCard"
 
 function BooksList ({ books, setBooks, setClickedBook, status, title, subtitle }) {
+
     if (books && books.error) {
         return (
             <div>{books.error.message}</div>
         )
     }
+    
     return (
         <>
         <div className="pt-4 pb-2 w-full border-x-0 border-t-0 border-y border-gray-200 gap-2">
             <h2 className="text-2xl">{title}</h2>
             <p className="text-xs text-gray-400">{subtitle}</p>
             <div className="mt-8 flex gap-6 w-[80vw] overflow-x-auto">
-                {books && books.filter(book => book.status == status).map((book) => 
+                {books && books
+                    .filter((book) => 
+                        book.status == status)
+                    .map((book) => 
                         <BookCard 
                             onClick={() => { setClickedBook(book) }}
                             book={book}
