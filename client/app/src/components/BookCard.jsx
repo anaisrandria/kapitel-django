04a100b92117841f4e87c509b3351a2633033c46
anaisrandria <anais.randriamitantsoa@gmail.com>
2025-02-03 18:14:21 +0100

    add: design bookcard wip

--- client/app/src/components/BookCard.jsx
+++ client/app/src/components/BookCard.jsx
@@ -17,19 +17,22 @@ function BookCard({ book, setBooks, setClickedBook, id, google_book_id, title, a
 
     return (
         <>
-        <div id="book-card" className="flex-shrink-0 flex flex-col mb-8 text-sm w-44" key={id}>
+        <div id="book-card" className="flex-shrink-0 flex flex-col mb-8 text-sm w-36" key={id}>
             <Link to={`./book/${google_book_id}`}>
-                <img src={book.image_link || "missingbook.jpg"} alt="book-cover" className="w-full max-h-64 drop-shadow-md rounded-sm mb-2"/>
+                <img src={book.image_link || "missingbook.jpg"} alt={title} className="w-36 h-56 drop-shadow-md rounded-md mb-2"/>
             </Link>
-            <p className="font-bold">{title}</p>
-            <p>{authors}</p>
-            <p>{release_year}</p>
-            <p>Status: {status}</p>
-            <p>Comments: {comments}</p>
-            <div id="buttons" className="flex flex-col">
+            <div id="title" className="flex justify-between">
+                <p className="font-bold truncate">{title}</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                    <path fill-rule="evenodd" d="M5.22 10.22a.75.75 0 0 1 1.06 0L8 11.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-2.25 2.25a.75.75 0 0 1-1.06 0l-2.25-2.25a.75.75 0 0 1 0-1.06ZM10.78 5.78a.75.75 0 0 1-1.06 0L8 4.06 6.28 5.78a.75.75 0 0 1-1.06-1.06l2.25-2.25a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06Z" clip-rule="evenodd" />
+                    </svg>
+            </div>
+            <p className="truncate">{authors}</p>
+            {/* <p>{release_year}</p> */}
+            {/* <div id="buttons" className="flex flex-col">
                 <button className="px-2 py-2 rounded-md my-1 border border-amber-700 bg-transparent text-amber-700 hover:bg-amber-700 hover:text-white">Modifier</button>
                 <button onClick={() => deleteBook(book.id)} className="px-2 py-2 my-1 rounded-md border bg-amber-700 text-white">Supprimer</button>
-            </div>
+            </div> */}
         </div>
         </>
     )
